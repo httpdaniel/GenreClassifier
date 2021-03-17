2112476c3aaa9ba5a9967d423065e85818df424c
Xuechun326 <73424895+Xuechun326@users.noreply.github.com>
2021-03-17 13:34:11 +0000

    Update lowercase&tokenization.py

--- data/data_transform/lowercase&tokenization.py
+++ data/data_transform/lowercase&tokenization.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import numpy as np
+import csv
+from nltk import word_tokenize
+column_names = ['Title', 'Artist', 'Featuring','Featured_Artist','Lyrics','Tags','Genre']
+data = pd.read_csv('../Genre/dataset.csv', names=column_names)
+X = data.iloc[:,4].values.astype('U')
+
+
+for i in range(10,100):
+    X[i] = str.lower(X[i])
+    a = word_tokenize(X[i])
+    with open('tokenization.csv', 'a', newline='', encoding='utf-8') as csvfile:  # open new csv file
+        writer = csv.writer(csvfile)  # create csv writer
+        writer.writerow([a]) # each lyrics per row
 
