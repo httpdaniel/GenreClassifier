bb60082b1daaaa991b9db88c83d7010517cdf225
httpdaniel <daniel.097@hotmail.com>
2021-04-01 12:29:57 +0100

    Adding complexity features

--- data/data_transform/lyric_complexity.py
+++ data/data_transform/lyric_complexity.py
@@ -1,3 +1,32 @@
-# TODO
+# Import packages
+import pandas as pd
+import nltk as nlp
+from nltk.tokenize import word_tokenize
 
-# Add features of lyrical complexity - word count, character count, variation in vocabulary, TF-IDF, etc.
+# Read in csv
+df = pd.read_csv('style.csv')
+
+# Lyric count
+df['Lyric_Count'] = df.Lyrics.apply(lambda x: len(str(x).split(' ')))
+
+# Character count
+df['Character_Count'] = df.Lyrics.apply(lambda x: len(str(x)))
+
+# POS count
+df['Noun_Count'] = df.Nouns.apply(lambda x: len(str(x).split(' ')))
+df['Verb_Count'] = df.Verbs.apply(lambda x: len(str(x).split(' ')))
+df['Adjective_Count'] = df.Adjectives.apply(lambda x: len(str(x).split(' ')))
+df['Adverb_Count'] = df.Adverbs.apply(lambda x: len(str(x).split(' ')))
+
+
+# Type token ratio
+def ttr(text):
+    word_tokens = word_tokenize(text)
+    types = nlp.Counter(word_tokens)
+    ratio = (len(types)/len(word_tokens))*100
+    return round(ratio, 2)
+
+
+df['TTR'] = df.Lyrics.apply(ttr)
+
+df.to_csv('complexity.csv', index=False, encoding='utf-8')
